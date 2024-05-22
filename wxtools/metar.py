e73e793a545199327da2dfa719a6957ca6207f73
Brian R <ricco19@gmail.com>
2024-05-22 16:08:15 -0400

    Refactoring...

--- wxtools/metar.py
+++ wxtools/metar.py
@@ -3,9 +3,10 @@ Experimenting with parsing METAR data into a python object.
 """
 
 from __future__ import annotations
+from datetime import datetime, timezone
 
-from .common import quotify
-from .units import _ALL_UNITS
+from .common import cardinal_direction, quotify, get_icao_name, fraction_str_to_float
+from .units import _ALL_UNITS, UnitInfo, convert_unit
 
 
 class CodedMetar:
@@ -215,95 +216,113 @@ class CodedMetar:
                 return remark
         return None
 
+    def decode(self) -> MetarObservations:
+        """
+        Uses this METAR and returns a MetarObservations object, which will aid
+        in decoding the string.
+        """
+        return MetarObservations(self)
+
+
+class MetarObservations:
+    """
+    Object for a observation stations data, specifically extracted and decoded
+    from a METAR.
+    """
+
+    def __init__(self, coded_metar: CodedMetar) -> None:
+        self.coded_metar = coded_metar
+        self.station_id = self.coded_metar.station_id
+        self.station_name = get_icao_name(self.coded_metar.station_id)
+        self.timestamp = self._parse_date_time(self.coded_metar.date_time)
+        self.wind = None
+        if self.coded_metar.wind is not None:
+            self.wind = MetarWind(self.coded_metar.wind, "mph")
+        self.visibility = MetarVisibility(self.coded_metar.visibility)
+        # Pressure, temp, sky coverage
+
+    def __repr__(self) -> str:
+        sb = f"{self.__class__.__name__}(\n"
+        sb = f"{sb}    coded_metar={quotify(self.coded_metar)},\n"
+        sb = f"{sb}    station_id={quotify(self.station_id)},\n"
+        sb = f"{sb}    station_name={quotify(self.station_name)},\n"
+        sb = f"{sb}    timestamp={quotify(self.timestamp)},\n"
+        sb = f"{sb}    wind={quotify(self.wind)},\n"
+        sb = f"{sb}    visibility={quotify(self.visibility)},\n"
+        return f"{sb})"
+
+    def __str__(self) -> str:
+        return repr(self)
+
+    @classmethod
+    def from_raw_string(cls, metar: str) -> MetarObservations:
+        """Constructs a MetarObservations object using just the raw METAR."""
+        return cls(CodedMetar(metar))
+
+    def _parse_date_time(self, date_group: str) -> datetime:
+        """
+        Note: the decoded version of this method will assume that the month
+        and year of the data is the current month and year.
+        """
+        metar_day_of_month = int(date_group[0:2])
+        metar_hour = int(date_group[2:4])
+        metar_minute = int(date_group[4:6])
+        current_dt = datetime.now(tz=timezone.utc)
+        metar_dt = datetime(
+            year=current_dt.year,
+            month=current_dt.month,
+            day=metar_day_of_month,
+            hour=metar_hour,
+            minute=metar_minute,
+            tzinfo=timezone.utc,
+        )
+        return metar_dt
+
+    def _minutes_since(self) -> int:
+        seconds = (datetime.now(tz=timezone.utc) - self.timestamp).seconds
+        return round(seconds / 60)
+
 
 class MetarWind:
-    """Object for parsing/decoding the wind group from a coded METAR."""
+    """
+    Object for parsing/decoding the wind group from a coded METAR.
+
+    Possible unit values (case insensitive):
+        * 'kt' -- Knots (default)
+        * 'mph' -- Miles per hour
+        * 'kph' -- Kilometers per hours
+        * 'mps' -- Meters per second
+    """
 
     _unit_kts = _ALL_UNITS["knot"]
     _unit_mph = _ALL_UNITS["mile per hour"]
     _unit_mps = _ALL_UNITS["meter per second"]
     _unit_kph = _ALL_UNITS["kilometer per hour"]
 
-    _CARDINAL_DIRECTIONS = (
-        "North",
-        "North-Northeast",
-        "Northeast",
-        "East-Northeast",
-        "East",
-        "East-Southeast",
-        "Southeast",
-        "South-Southeast",
-        "South",
-        "South-Southwest",
-        "Southwest",
-        "West-Southwest",
-        "West",
-        "West-Northwest",
-        "Northwest",
-        "North-Northwest",
-    )
-
-    _CARDINAL_DIRECTIONS_ARROW = (
-        "⬇",
-        "⬇",
-        "⬋",
-        "⬅",
-        "⬅",
-        "⬅",
-        "⬉",
-        "⬆",
-        "⬆",
-        "⬆",
-        "⬈",
-        "➡",
-        "➡",
-        "➡",
-        "⬊",
-        "⬇",
-    )
-
-    _CARDINAL_DIRECTIONS_ABBR = (
-        "N",
-        "NNE",
-        "NE",
-        "ENE",
-        "E",
-        "ESE",
-        "SE",
-        "SSE",
-        "S",
-        "SSW",
-        "SW",
-        "WSW",
-        "W",
-        "WNW",
-        "NW",
-        "NNW",
-    )
-
-    def __init__(self, metar_wind_group: str) -> None:
+    def __init__(self, metar_wind_group: str, unit: str = "kt") -> None:
         self.wind_group = metar_wind_group.upper()
         # Default values indicate calm wind
-        self.speed: int = 0
-        self.gust: int | None = None
+        self.speed: float = 0
+        self.gust: float | None = None
         self.direction: int | None = None
         self.variable_directions: tuple[int, int] | None = None
         # Parse the string
         if self.wind_group.startswith("VRB"):
             # Variable wind < 6kts, indicated by keeping direction None
-            self.speed = int(self.wind_group[3:5])
+            self.speed = float(self.wind_group[3:5])
         elif self.wind_group != "00000KT":
             groups = self.wind_group.split()
             gust_spl = groups[0][0:-2].split("G")
             self.direction = int(gust_spl[0][0:3])
-            self.speed = int(gust_spl[0][3:])
+            self.speed = float(gust_spl[0][3:])
             if len(gust_spl) > 1:
-                self.gust = int(gust_spl[1])
+                self.gust = float(gust_spl[1])
             if len(groups) > 1:
                 var_spl = groups[1].split("V")
                 self.variable_directions = (int(var_spl[0]), int(var_spl[1]))
-        # METAR is always knots, so set that as our speed unit to start
-        self.speed_unit = self._unit_kts
+        # Set the unit and convert if required
+        self._speed_unit = self._unit_kts
+        self._convert_unit(self._uinfo_from_str(unit))
 
     def __repr__(self) -> str:
         sb = f"{self.__class__.__name__}(\n"
@@ -318,6 +337,26 @@ class MetarWind:
     def __str__(self) -> str:
         return self.description()
 
+    def _uinfo_from_str(self, unit: str) -> UnitInfo:
+        unit_str = unit.lower().strip()
+        if unit_str == "kt":
+            return self._unit_kts
+        if unit_str == "mph":
+            return self._unit_mph
+        if unit_str == "kph":
+            return self._unit_kph
+        if unit_str == "mps":
+            return self._unit_mps
+        raise RuntimeError(f"Invalid unit specified: '{unit}'")
+
+    def _convert_unit(self, to_unit: UnitInfo) -> None:
+        if to_unit == self._speed_unit:
+            return
+        self.speed = convert_unit(self.speed, self._speed_unit, to_unit)
+        if self.gust is not None:
+            self.gust = convert_unit(self.gust, self._speed_unit, to_unit)
+        self._speed_unit = to_unit
+
     def description(self) -> str:
         """
         Outputs a human readable description of the decoded wind observations.
@@ -325,16 +364,40 @@ class MetarWind:
         if self.speed == 0 and self.gust is None:
             return "Calm"
         if self.direction is None:
-            return f"{self.speed} {self.speed_unit} from varying directions"
-        sb = f"{self.speed} {self.speed_unit}"
-        sb = f"{sb} from the {self.cardinal_direction(self.direction)}"
+            return f"{self.speed:.0f} {self.speed_unit} from varying directions"
+        sb = f"{self.speed:.0f} {self.speed_unit}"
+        sb = f"{sb} from the {cardinal_direction(self.direction)}"
         if self.gust is not None:
-            sb = f"{sb}, gusting {self.gust} {self.speed_unit}"
+            sb = f"{sb}, gusting {self.gust:.0f} {self.speed_unit}"
         if self.variable_directions is not None:
-            v1 = self.cardinal_direction(self.variable_directions[0])
-            v2 = self.cardinal_direction(self.variable_directions[1])
+            v1 = cardinal_direction(self.variable_directions[0])
+            v2 = cardinal_direction(self.variable_directions[1])
             sb = f"{sb}, varying from {v1} and {v2}"
         return sb
+
+    @property
+    def speed_unit(self) -> UnitInfo:
+        """
+        The current unit (UnitInfo object) of the wind speeds. If set to one
+        of the below strings, will convert the values in place and set the new
+        UnitInfo appropriately.
+
+        Possible set values (case insensitive):
+        * 'kt' -- Knots (default from METAR)
+        * 'mph' -- Miles per hour
+        * 'kph' -- Kilometers per hours
+        * 'mps' -- Meters per second
+        """
+        return self._speed_unit
+
+    @speed_unit.setter
+    def speed_unit(self, unit: str) -> None:
+        to_unit = self._uinfo_from_str(unit)
+        from_unit = self._speed_unit
+        self.speed = convert_unit(self.speed, from_unit, to_unit)
+        if self.gust is not None:
+            self.gust = convert_unit(self.gust, from_unit, to_unit)
+        self._speed_unit = to_unit
 
     @classmethod
     def from_coded_metar(cls, metar: CodedMetar) -> MetarWind | None:
@@ -347,34 +410,91 @@ class MetarWind:
             return None
         return cls(metar.wind)
 
-    @staticmethod
-    def cardinal_direction(direction: int, style: str = "shortarrow") -> str:
-        """
-        The cardinal direction of the specified wind direction value.
 
-        Parameters:
-        * direction (int) -- Direction of wind in 0-360 degrees.
-        * style (str) -- The style of string to be returned. Possible values
-        are 'short', 'long', 'arrow', 'shortarrow', 'degrees'. Defaults to
-        'shortarrow'.
+class MetarVisibility:
+    """
+    Object for parsing/decoding the visibility group from a coded METAR.
 
-        Examples of each style for northeasterly wind:
-        * 'short' -> 'NE'
-        * 'long' -> 'Northeast'
-        * 'arrow' -> '⬋'
-        * 'shortarrow -> 'NE ⬋'
-        * 'degrees' -> '45°'
+    Possible unit values (case insensitive):
+        * 'miles' -- Statuate miles (default)
+        * 'ft' -- Feet
+    """
+
+    _unit_miles = _ALL_UNITS["mile us statute"]
+    _unit_ft = _ALL_UNITS["foot"]
+
+    def __init__(self, metar_vis_group: str, unit: str = "miles") -> None:
+        self.visibility_group = metar_vis_group.upper().strip()
+        if self.visibility_group[0] == "M":
+            self.distance = fraction_str_to_float(self.visibility_group[1:-2])
+            self.less_than_flag = True
+        else:
+            self.distance = fraction_str_to_float(self.visibility_group[0:-2])
+            self.less_than_flag = False
+        # Set the unit and convert if required
+        self._unit = self._unit_miles
+        self._convert_unit(self._uinfo_from_str(unit))
+
+    def __repr__(self) -> str:
+        sb = f"{self.__class__.__name__}(\n"
+        sb = f"{sb}    visibility_group={quotify(self.visibility_group)},\n"
+        sb = f"{sb}    distance={quotify(self.distance)},\n"
+        sb = f"{sb}    less_than_flag={quotify(self.less_than_flag)},\n"
+        sb = f"{sb}    unit={quotify(self.unit)},\n"
+        return f"{sb})"
+
+    def __str__(self) -> str:
+        return self.description()
+
+    def _uinfo_from_str(self, unit: str) -> UnitInfo:
+        unit_str = unit.lower().strip()
+        if unit_str == "miles":
+            return self._unit_miles
+        if unit_str == "ft":
+            return self._unit_ft
+        raise RuntimeError(f"Invalid unit specified: '{unit}'")
+
+    def _convert_unit(self, to_unit: UnitInfo) -> None:
+        if to_unit == self._unit:
+            return
+        self.distance = convert_unit(self.distance, self._unit, to_unit)
+        self._unit = to_unit
+
+    def description(self) -> str:
         """
-        cfstyle = style.casefold()
-        cardinal_index = int(round(direction / 22.5) % 16)
-        if cfstyle == "arrow":
-            return MetarWind._CARDINAL_DIRECTIONS_ARROW[cardinal_index]
-        if cfstyle == "long":
-            return MetarWind._CARDINAL_DIRECTIONS[cardinal_index]
-        if cfstyle == "degrees":
-            return f"{direction}°"
-        if cfstyle == "shortarrow":
-            arrow = MetarWind._CARDINAL_DIRECTIONS_ARROW[cardinal_index]
-            abbr = MetarWind._CARDINAL_DIRECTIONS_ABBR[cardinal_index]
-            return f"{abbr} {arrow}"
-        return MetarWind._CARDINAL_DIRECTIONS_ABBR[cardinal_index]
+        Outputs a human readable description of the decoded wind observations.
+        """
+        distance_str = f"{self.distance:.2f}"
+        if self.unit.label == "foot":
+            distance_str = f"{self.distance:.0f}"
+        if self.less_than_flag:
+            return f"Less than {distance_str} {self.unit}"
+        return f"{distance_str} {self.unit}"
+
+    @property
+    def unit(self) -> UnitInfo:
+        """
+        The current unit (UnitInfo object) of the wind speeds. If set to one
+        of the below strings, will convert the values in place and set the new
+        UnitInfo appropriately.
+
+        Possible unit values (case insensitive):
+            * 'miles' -- Statuate miles (default)
+            * 'ft' -- Feet
+        """
+        return self._unit
+
+    @unit.setter
+    def unit(self, unit: str) -> None:
+        to_unit = self._uinfo_from_str(unit)
+        from_unit = self._unit
+        self.distance = convert_unit(self.distance, from_unit, to_unit)
+        self._unit = to_unit
+
+    @classmethod
+    def from_coded_metar(cls, metar: CodedMetar) -> MetarVisibility:
+        """
+        Creates a decoded MetarVisibility object from a CodedMetar. This will
+        simply use CodedMetar.visibility to construct the object.
+        """
+        return cls(metar.visibility)
